bcc4fa71009219dc8eede9d8eb454324635aac23
UmiTello <student.00010402@gmail.com>
2021-04-04 02:34:59 +0500

    create function

--- app.js
+++ app.js
@@ -18,14 +18,57 @@ app.get('/', (req, res) => {
 app.get('/addemployee', (req, res) => {
     res.render('addemployee')
 })
+app.post('/addemployee', (req, res) => {
+
+    const name = req.body.name
+    const surname = req.body.surname
+    const address = req.body.address
+    const dob = req.body.dob
+    const briefdesc = req.body.briefdesc
+    const job = req.body.job
+    // const image = req.body.image.filename
+
+    fs.readFile('./data/employees.json', (err, data) => {
+        if (err) throw err
+
+        const employees = JSON.parse(data)
+
+        employees.push({
+            id: idgenerator(),
+            em_name: name,
+            em_surname: surname,
+            em_address: address,
+            em_dob: dob,
+            em_briefstory: briefdesc,
+            em_job: job,
+            // em_img: image
+        })
+
+        fs.writeFile('./data/employees.json', JSON.stringify(employees), err => {
+            if (err) throw err
+
+
+            res.render('addemployee', { success: true })
+        })
+    })
+})
+
+
+const employees = ['em1', 'em2']
 
 app.get('/allemployees', (req, res) => {
-    res.render('allemployees')
+    res.render('allemployees', { employee: employees })
 })
+
+
+
 
 app.get('/contact', (req, res) => {
     res.render('contact')
 })
+
+
+
 
 app.get('/employeedetail', (req, res) => {
     res.render('employeedetail')
@@ -90,13 +133,13 @@ app.get('/employeedetail', (req, res) => {
 //     })
 // })
 
-app.listen(8000, err => {
+app.listen(5050, err => {
     if (err) throw err
 
-    console.log('App is running on port 8000...')
+    console.log('App is running on port 5050...')
 })
 
 
-function id() {
+function idgenerator() {
     return '_' + Math.random().toString(36).substr(2, 9);
 }
